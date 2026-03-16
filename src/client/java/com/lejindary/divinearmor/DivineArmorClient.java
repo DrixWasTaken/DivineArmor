97e6c5d05cae5352861fde0f2954c218c774749e
DrixWasTaken <superdrix09@gmail.com>
2026-03-16 13:52:33 -0700

    inint

---
+++ src/client/java/com/lejindary/divinearmor/DivineArmorClient.java
@@ -0,0 +1,10 @@
+package com.lejindary.divinearmor;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DivineArmorClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
