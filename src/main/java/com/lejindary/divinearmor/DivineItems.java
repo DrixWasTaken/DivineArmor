147b179b6d796295f21c4dab7cf70f6d57654e10
skyridgemyriadstudios-cpu <skyridgemyriadstudios@gmail.com>
2026-03-17 12:17:01 -0700

    mske itrm

---
+++ src/main/java/com/lejindary/divinearmor/DivineItems.java
@@ -0,0 +1,26 @@
+package com.lejindary.divinearmor;
+
+import com.google.common.base.Function;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.world.item.Item;
+
+public class DivineItems {
+	public static <T extends Item> T register(String name, Function<Item.Properties, T> itemFactory, Item.Properties settings) {
+		// Create the item key.
+		ResourceKey<Item> itemKey = ResourceKey.create(Registries.ITEM, Identifier.fromNamespaceAndPath(DivineArmor.MOD_ID, name));
+
+		// Create the item instance.
+		T item = itemFactory.apply(settings.setId(itemKey));
+
+		// Register the item.
+		Registry.register(BuiltInRegistries.ITEM, itemKey, item);
+
+		return item;
+	}
+
+}
